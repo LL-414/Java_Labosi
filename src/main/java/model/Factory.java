f72b6f7dc047ea4567f086bb6495761695b4cdf3
Lukas Luksic <lluksic20@gmail.com>
2023-10-13 15:41:14 +0200

    Added prodution main/model packages. Added objects, getters and setters, constructors to classes.ž Added classes to package model:Factory, Store, Address, Item, Category.

---
+++ src/main/java/model/Factory.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Factory {
+    private String name;
+    private Item[] items;
+    private Address address;
+
+    public Factory(String name, Item[] items, Address address) {
+        this.name = name;
+        this.items = items;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Item[] getItems() {
+        return items;
+    }
+
+    public void setItems(Item[] items) {
+        this.items = items;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
---
+++ src/main/java/model/Store.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Store {
+    private String name, webAddress;
+    private Item[] items;
+
+    public Store(String name, String webAddress, Item[] items) {
+        this.name = name;
+        this.webAddress = webAddress;
+        this.items = items;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebAddress() {
+        return webAddress;
+    }
+
+    public void setWebAddress(String webAddress) {
+        this.webAddress = webAddress;
+    }
+
+    public Item[] getItems() {
+        return items;
+    }
+
+    public void setItems(Item[] items) {
+        this.items = items;
+    }
+}
